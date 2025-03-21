1cf129307803162456575b98199b02e2bc6869c8
RenatoPython <ratohardcore@hotmail.com>
2025-03-20 21:01:34 -0300

    lista 4 exercício 6

---
+++ lista4/exercicio6resposta.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resposta de Funções</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Exemplo de uso de Funções - Resposta 6</h1>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $numero = $_POST["numero"];
+    
+        if (is_numeric($numero)) {
+            $arredondado = round($numero);
+            echo "<p>O número $numero arredondado é $arredondado.</p>";
+        } else {
+            echo "<p>Por favor, insira um número válido.</p>";
+        }
+    }
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
