fe8989fdac160464d4b99dddef796f4cc493ad9c
RenatoPython <ratohardcore@hotmail.com>
2025-03-20 21:01:21 -0300

    lista 4 exercício 5

---
+++ lista4/exercicio5.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Funções Aula 06</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Exemplo de uso de Funções - Formulário 4</h1>
+
+    <form method="post" action="exercicio5resposta.php">
+
+        <div class="mb-3">
+        <label for="numero">Digite um número:</label>
+        <input type="number" name="numero" id="numero" step="any" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Calcular</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
