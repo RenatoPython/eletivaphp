69cd6036a78c047a6e15cc51a8a775badc4498ff
RenatoPython <ratohardcore@hotmail.com>
2025-03-20 21:00:49 -0300

    lista 4 exercício 3

---
+++ lista4/exercicio3.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Funções Aula 06</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Exemplo de uso de Funções - Formulário 4</h1>
+
+    <form method="post" action="exercicio3resposta.php">
+
+        <label for="palavra1">Digite a primeira palavra:</label>
+        <input type="text" name="palavra1" id="palavra1" required>
+        <br>
+        <label for="palavra2">Digite a segunda palavra:</label>
+        <input type="text" name="palavra2" id="palavra2" required>
+        <br>
+        
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
